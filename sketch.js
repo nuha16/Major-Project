74ad29d5e0d5c95f7a6d6a8163b32855e331f523
nuha16 <114016122+nuha16@users.noreply.github.com>
2022-11-29 11:50:28 -0600

    preloading images

--- sketch.js
+++ sketch.js
@@ -5,11 +5,20 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let pinkBackground, pinkButton, blueButton, brownButton, greenButton;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function preload(){
+  pinkBackground = loadImage("images/pink cloud background.png");
+  pinkButton = loadImage("images/pink strip.png");
+  blueButton = loadImage("images/blue strip.png");
+  brownButton = loadImage("images/brown strip.png");
+  greenButton = loadImage("images/green strip.png");
+}
+
 function draw() {
-  background(220);
+  image(pinkBackground, 0, 0, windowWidth*1.0002, windowHeight*1.02);
 }

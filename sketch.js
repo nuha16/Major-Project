55dbd1faec82fb8b3a9c601ad4c6389506ccdcfc
nuha16 <114016122+nuha16@users.noreply.github.com>
2023-01-12 12:47:23 -0600

    problem solved

--- sketch.js
+++ sketch.js
@@ -10,74 +10,64 @@
 // an extension was needed to be downloaded for the fonts - stxr.iconfont-preview
 
 
-// code keeps crashing so I'm going to rewrite
-// let pinkButton, blueButton, brownButton, greenButton, purplePillars, rainyHearts, enchantedSword, littleLego;
-// let state = "start";
+let pinkButton, blueButton, brownButton, greenButton, purplePillars, rainyHearts, enchantedSword, littleLego;
 
 // // questions to ask the player
 // let questions = {};
 
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-// }
+let state = "start";
 
-// function preload(){
-//   // fonts
-//   rainyHearts = loadFont("fonts/rainyhearts.ttf");
-//   enchantedSword = loadFont("fonts/EnchantedSword.ttf");
-//   littleLego = loadFont("fonts/littlelego.ttf");
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+}
 
-//   // images
-//   purplePillars = loadImage("purple pillars.png");
-//   pinkButton = loadImage("images/pink strip.png");
-//   blueButton = loadImage("images/blue strip.png");
-//   brownButton = loadImage("images/brown strip.png");
-//   greenButton = loadImage("images/green strip.png");
-// }
+function preload(){
+  // images
+  purplePillars = loadImage("images/purple pillars.png");
+  pinkButton = loadImage("images/pink strip.png");
+  blueButton = loadImage("images/blue strip.png");
+  brownButton = loadImage("images/brown strip.png");
+  greenButton = loadImage("images/green strip.png");
 
-// function draw() {
-//   if (state === "start"){
-//     startBackground();
-//   }
+  // fonts
+  littleLego = loadFont("fonts/littlelego.ttf");
+  rainyHearts = loadFont("fonts/rainyhearts.ttf");
+  enchantedSword = loadFont("fonts/EnchantedSword.ttf");
+  littleLego = loadFont("fonts/littlelego.ttf");
 
-//   // if (state === "menu"){
-//   //   menuBackground();
-//   // }
-// }
+}
 
-// class Button {
-//   constructor(x, y, width, height, img){
-//     this.x = x;
-//     this.y = y;
-//     this.width = width;
-//     this.height = height;
-//     this.img = img;
-//   }
+function draw(){
+  startBackground();
+}
 
-//   display() {
-//     image(this.img, this.x, this.y, this.width, this.height);
-//   }
+class Button {
+  constructor(x, y, width, height, img){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.img = img;
+  }
 
-//   insideButton(x, y) {
-//     return x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height;
-//   }
-// }
+  display() {
+    image(this.img, this.x, this.y, this.width, this.height);
+  }
 
-// // function menuBackground() {
-// //   // background for main menu
-// //   imageMode(CENTER);
-// //   image(width/2, height/2, windowWidth*0.9, windowHeight*0.999);
-// // }
+  insideButton(x, y) {
+    return x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height;
+  }
+}
 
-// function startBackground() {
-//   // background for start
-//   background("#a393fa");
+function startBackground() {
+  // background for start
+  background("#a393fa");
 
-//   imageMode(CENTER);
-//   image(purplePillars, width/2, height/2, width*0.5, height*0.999);
+  imageMode(CENTER);
+  image(purplePillars, width/2, height/2, width*0.5, height*0.999);
 
-//   textSize(50);
-//   textFont(littleLego);
-//   text("TITLE", 100, 200);
-//   text("random words", 200, 100);
-// }
+  textSize(50);
+  textFont(littleLego);
+  text("TITLE", 100, 200);
+  text("random words", 200, 100);
+}
